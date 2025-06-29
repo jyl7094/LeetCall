48e5c64d93e4c8d563b5aed2161a3db600fff35f
jyl7094 <jyl7094@gmail.com>
2025-06-29 04:35:03 -0400

    Remove mutation observer from problemset.ts

--- src/content/problemset.ts
+++ src/content/problemset.ts
@@ -2,24 +2,5 @@ function logHelloWorld() {
   console.log("hello world");
 }
 
-function observePage() {
-  const observer = new MutationObserver((mutations) => {
-    for (const mutation of mutations) {
-      for (const node of mutation.addedNodes) {
-        if (node.nodeType === Node.ELEMENT_NODE) {
-          logHelloWorld();
-        }
-      }
-    }
-  });
-
-  observer.observe(document.body, {
-    childList: true,
-    subtree: true,
-  });
-
-  // Initial run
-  logHelloWorld();
-}
-
-observePage();
+// Initial run
+logHelloWorld();
