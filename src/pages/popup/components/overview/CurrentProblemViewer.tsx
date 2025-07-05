4c4abcee98f9d204c3d8b6eea55649fb4f8a6c69
jyl7094 <jyl7094@gmail.com>
2025-07-05 16:41:48 -0400

    refactor: extract current problem status viewer to a component

---
+++ src/pages/popup/components/overview/CurrentProblemViewer.tsx
@@ -0,0 +1,25 @@
+import type { Problem } from "@/types/problem";
+
+const CurrentProblemViewer = ({
+  currentProblem,
+  solvedProblems,
+}: {
+  currentProblem: Problem | null;
+  solvedProblems: Set<string>;
+}) => (
+  <div className="space-y-2">
+    <div className="font-medium">Current Problem</div>
+    {currentProblem ? (
+      <div className="border border-gray-200 rounded-lg text-xs px-3 py-2.5 inline-flex items-center justify-between w-full text-left">
+        {currentProblem.title}
+        {solvedProblems.has(currentProblem.id) && (
+          <div className="text-emerald-600 font-medium">Completed</div>
+        )}
+      </div>
+    ) : (
+      <div className="text-gray-500">No active problem</div>
+    )}
+  </div>
+);
+
+export default CurrentProblemViewer;
--- src/pages/popup/components/screens/OverviewScreen.tsx
+++ src/pages/popup/components/screens/OverviewScreen.tsx
@@ -2,6 +2,7 @@ import { useUrlChange } from "@/hooks/useUrlChange";
 import ProblemButton from "@/pages/popup/components/overview/ProblemButton";
 import type { Problem } from "@/types/problem";
 import { useCallback, useEffect, useState } from "react";
+import CurrentProblemViewer from "../overview/CurrentProblemViewer";
 
 const OverviewScreen = () => {
   const [currentProblem, setCurrentProblem] = useState<Problem | null>(null);
@@ -342,22 +343,6 @@ const OverviewScreen = () => {
     );
   };
 
-  const renderCurrentProblem = () => (
-    <div className="space-y-2">
-      <div className="font-medium">Current Problem</div>
-      {currentProblem ? (
-        <div className="border border-gray-200 rounded-lg text-xs px-3 py-2.5 inline-flex items-center justify-between w-full text-left">
-          {currentProblem.title}
-          {solvedProblems.has(currentProblem.id) && (
-            <div className="text-emerald-600 font-medium">Completed</div>
-          )}
-        </div>
-      ) : (
-        <div className="text-gray-500">No active problem</div>
-      )}
-    </div>
-  );
-
   const renderRatingButtons = () => {
     const disabled = !currentProblem || solvedProblems.has(currentProblem.id);
     return (
@@ -462,7 +447,10 @@ const OverviewScreen = () => {
       <div className="flex items-center justify-center border-b pb-4 border-0 border-gray-200">
         {renderStatus()}
       </div>
-      {renderCurrentProblem()}
+      <CurrentProblemViewer
+        currentProblem={currentProblem}
+        solvedProblems={solvedProblems}
+      />
       {renderRatingButtons()}
       {renderUpcoming()}
     </div>
