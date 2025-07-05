51079bac3e333fe8ce6ad5396af3585c5b991313
jyl7094 <jyl7094@gmail.com>
2025-07-05 19:24:26 -0400

    refacotr: export default ScreenState

--- src/constants/screenState.ts
+++ src/constants/screenState.ts
@@ -1,7 +1,9 @@
-export const ScreenState = {
+const ScreenState = {
   Loading: "LOADING",
   Instructions: "INSTRUCTIONS",
   Overview: "OVERVIEW",
 } as const;
 
 export type ScreenState = (typeof ScreenState)[keyof typeof ScreenState];
+
+export default ScreenState;
