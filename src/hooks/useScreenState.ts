e9538c0252f54e73e682bf86375dbd90eaf6e108
jyl7094 <jyl7094@gmail.com>
2025-07-23 14:19:26 -0400

    feat: change screen to show overview even when there are no problems saved and is viewing a problem

--- src/hooks/useScreenState.ts
+++ src/hooks/useScreenState.ts
@@ -1,25 +1,31 @@
 import { ScreenState } from "@/constants/screenState";
 import { useEffect, useState } from "react";
+import { useUrlChange } from "./useUrlChange";
 
 export const useScreenState = () => {
   const [screen, setScreen] = useState<ScreenState>(ScreenState.Loading);
+  const url = useUrlChange();
 
   useEffect(() => {
     const loadingScreenState = async () => {
       try {
         const result = await chrome.storage.local.get(["problems"]);
         const problems = result.problems || [];
-        setScreen(
-          problems.length === 0
-            ? ScreenState.Instructions
-            : ScreenState.Overview,
-        );
+        if (url.startsWith("https://leetcode.com/problems/")) {
+          setScreen(ScreenState.Overview);
+        } else {
+          setScreen(
+            problems.length === 0
+              ? ScreenState.Instructions
+              : ScreenState.Overview,
+          );
+        }
       } catch {
         setScreen(ScreenState.Instructions);
       }
     };
     loadingScreenState();
-  }, []);
+  }, [url]);
 
   return [screen, setScreen] as const;
 };
