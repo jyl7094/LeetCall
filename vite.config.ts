94266361b8b7348b3410d5fd22cedb91a3ec1db6
jyl7094 <jyl7094@gmail.com>
2025-07-23 13:25:20 -0400

    fix: update vite roolup options to output content-script

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
       input: {
         popup: resolve(__dirname, "src/pages/popup/index.html"),
         dashboard: resolve(__dirname, "src/pages/dashboard/index.html"),
-        contentScript: resolve(__dirname, "src/content/content-script.ts"),
+        "content-script": resolve(__dirname, "src/content/content-script.ts"),
         background: resolve(__dirname, "src/background/background.ts"),
       },
       output: {
